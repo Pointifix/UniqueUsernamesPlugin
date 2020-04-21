c6a953df914fd5f00cbb1ee9a275e69c581b31ba
Simon Pointner <simon.pointner@gmx.at>
2020-04-21 03:47:39 +0200

    database communcation and player handling

--- src/main/java/unique/usernames/Database.java
+++ src/main/java/unique/usernames/Database.java
@@ -1,7 +1,78 @@
 package unique.usernames;
 
-public class Database {
-    public Database() {
+import arc.util.Log;
+import mindustry.entities.type.Player;
 
+import java.sql.*;
+import java.util.HashMap;
+
+public class Database {
+    private Connection connection;
+
+    public Database(String driver, String url, String username, String password) {
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, username, password);
+            PreparedStatement ps = connection.prepareStatement("create table if not exists unique_usernames(id int identity(1,1) primary key, uuid varchar(255), username varchar(255))");
+            ps.execute();
+            ps.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public HashMap<String, String> readAllPlayers() {
+        HashMap<String, String> result = new HashMap<>();
+        try {
+            PreparedStatement ps = connection.prepareStatement("select * from unique_usernames");
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                result.put(rs.getString(2), rs.getString(3));
+            }
+
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public void addPlayer(Player player) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("insert into unique_usernames(uuid, username) values (?, ?)");
+            ps.setString(1, player.uuid);
+            ps.setString(2, player.name);
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updatePlayer(Player player) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("update unique_usernames set username = ? where uuid = ?");
+            ps.setString(1, player.name);
+            ps.setString(2, player.uuid);
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean deletePlayer(String username) {
+        int rows = 0;
+        try {
+            PreparedStatement ps = connection.prepareStatement("delete from unique_usernames where username = ?");
+            ps.setString(1, username);
+            rows = ps.executeUpdate();
+
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rows > 0;
     }
 }
--- src/main/java/unique/usernames/UniqueUsernamesPlugin.java
+++ src/main/java/unique/usernames/UniqueUsernamesPlugin.java
@@ -1,16 +1,74 @@
 package unique.usernames;
 
+import arc.Events;
 import arc.util.CommandHandler;
+import arc.util.Log;
+import mindustry.Vars;
+import mindustry.entities.type.Player;
+import mindustry.game.EventType;
+import mindustry.net.Packets;
 import mindustry.plugin.Plugin;
 
-public class UniqueUsernamesPlugin extends Plugin {
-    public UniqueUsernamesPlugin(){
+import java.util.HashMap;
+import java.util.Map;
 
+public class UniqueUsernamesPlugin extends Plugin {
+    Config config;
+    Database database;
+
+    public HashMap<String, String> uniqueUsernames = new HashMap<>();
+
+    public UniqueUsernamesPlugin() {
+        config = new Config();
+
+        database = new Database(config.getString("driver"), config.getString("url"), config.getString("username"), config.getString("password"));
+
+        uniqueUsernames.putAll(database.readAllPlayers());
+
+        Events.on(EventType.PlayerJoin.class, playerJoin -> {
+            Player player = playerJoin.player;
+
+            if (uniqueUsernames.get(player.uuid) == null) {
+                if (uniqueUsernames.containsValue(player.name)) {
+                    player.con.kick(Packets.KickReason.nameInUse);
+                } else {
+                    uniqueUsernames.put(player.uuid, player.name);
+                    database.addPlayer(player);
+                }
+            }
+            if (uniqueUsernames.get(player.uuid) != player.name) {
+                if (uniqueUsernames.containsValue(player.name)) {
+                    player.con.kick(Packets.KickReason.nameInUse);
+                } else {
+                    uniqueUsernames.replace(player.uuid, player.name);
+                    database.updatePlayer(player);
+                }
+            }
+        });
     }
 
     @Override
     public void registerServerCommands(CommandHandler handler){
+        handler.register("freeusername", "<username>", "List all thorium reactors in the map.", args -> {
+            String username = args[0];
 
+            for (Player player : Vars.playerGroup.all()) {
+                if (player.name.equals(username)) player.con.kick(Packets.KickReason.nameInUse);
+            }
+            for (Map.Entry<String, String> entry : uniqueUsernames.entrySet()) {
+                if (entry.getValue().equals(username)) {
+                    uniqueUsernames.remove(entry.getKey());
+                    break;
+                }
+            }
+
+
+            if(database.deletePlayer(username)) {
+                Log.info("Successfully freed up username " + username + ".");
+            } else {
+                Log.info("Username " + username + " was not occupied.");
+            }
+        });
     }
 
     @Override
